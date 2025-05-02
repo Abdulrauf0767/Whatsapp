581f34b4d0b91e3682a3bf07232a1cfb5b648a19
Abdulrauf0767 <araof1803@mail.com>
2025-05-02 14:42:39 +0500

    change input width

--- src/Components/ChatBox.jsx
+++ src/Components/ChatBox.jsx
@@ -588,10 +588,10 @@ const ChatBox = ({ selectedChat, onBack, isMobile }) => {
           </div>
         ) : (
           <div className='w-full flex items-center justify-between bg-[#f0f2f5] rounded-lg px-3 py-2'>
-            <div className='flex items-center gap-x-3'>
+            <div className='flex items-center gap-x-3 w-[25%] '>
               <button 
                 onClick={toggleEmojiPicker}
-                className='text-gray-500 hover:text-gray-700'
+                className='text-gray-500 hover:text-gray-700 '
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
@@ -599,7 +599,7 @@ const ChatBox = ({ selectedChat, onBack, isMobile }) => {
               </button>
               <button 
                 onClick={openFileSelector}
-                className='text-gray-500 hover:text-gray-700'
+                className='text-gray-500 hover:text-gray-700  '
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
@@ -654,7 +654,7 @@ const ChatBox = ({ selectedChat, onBack, isMobile }) => {
                 onChange={(e) => setMessage(e.target.value)}
                 onKeyPress={handleKeyPress}
                 placeholder='Type a message' 
-                className='flex-1 bg-transparent outline-none text-sm px-4 py-2 mx-2 rounded-lg' 
+                className='w-[50%] bg-transparent outline-none text-sm px-4 py-2 mx-2 rounded-lg' 
               />
             )}
             
@@ -665,7 +665,7 @@ const ChatBox = ({ selectedChat, onBack, isMobile }) => {
                 </svg>
               </button>
             ) : (
-              <button onClick={startRecording} className={`text-gray-500 hover:text-gray-700 ${isRecording ? 'text-red-500' : ''}`}>
+              <button onClick={startRecording} className={`text-gray-500 w-[10%] hover:text-gray-700 ${isRecording ? 'text-red-500' : ''}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
                 </svg>
